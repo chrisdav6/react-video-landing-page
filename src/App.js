a11d0ad7d7c9fd64ccff79b785d7f199d166af26
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-01-13 08:53:52 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import explore from './img/explore.mp4';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <video className='video' src={explore} autoPlay muted></video>
     </div>
   );
-}
+};
 
 export default App;
